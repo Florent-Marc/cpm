0b2dcbfe49fdf303194fe5d0a218ba992748c595
MK <florentmarc6@gmail.com>
2024-05-27 15:16:24 +0200

    Add save for block

--- src/main/java/com/mk/cpm/BlockController.java
+++ src/main/java/com/mk/cpm/BlockController.java
@@ -1,6 +1,8 @@
 package com.mk.cpm;
 
+import com.mk.cpm.config.Config;
 import com.mk.cpm.loader.Block;
+import com.mk.cpm.loader.LoaderBlock;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +13,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,16 +28,21 @@ public class BlockController implements Initializable {
     public TextField model;
     public TextField CreativeTab;
     public ChoiceBox choiceBox;
+    public TextField itemrotation;
+    public TextField itemtranslation;
+    public TextField itemscale;
+    public TextField IconText;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        if (isprop.isSelected()) {
+        Block block = MainController.blockselected;
+        if (block.getEmptyMass() !=null) {
             tabprop.setDisable(false);
+            isprop.setSelected(true);
         } else {
             tabprop.setDisable(true);
         }
-        Block block = MainController.blockselected;
         if (block.getCreativeTab() != null) {
             CreativeTab.setText(block.getCreativeTab());
         }
@@ -46,6 +55,15 @@ public class BlockController implements Initializable {
         if (block.getName() != null) {
             name.setText(block.getName());
         }
+        if (block.getItemRotation() != null) {
+            itemrotation.setText(block.getItemRotation());
+        }
+        if (block.getItemTranslation() != null) {
+            itemtranslation.setText(block.getItemTranslation());
+        }
+        if (block.getItemScale() != null) {
+            itemscale.setText(block.getItemScale());
+        }
         choiceBox.getItems().addAll("NONE","WORLD","ALL");
     }
 
@@ -55,6 +73,41 @@ public class BlockController implements Initializable {
             tabprop.setDisable(false);
         } else {
             tabprop.setDisable(true);
+        }
+    }
+
+    @FXML
+    public void save(MouseEvent mouseEvent) {
+        File f = LoaderBlock.getFile(MainController.blockselected.getName());
+        if (f == null) {
+            return;
+        }
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write("Name: " + name.getText() + "\n");
+            fw.write("Description: " + desc.getText() + "\n");
+            fw.write("Model: " + model.getText() + "\n");
+            if(!CreativeTab.getText().isEmpty()){
+                fw.write("CreativeTab: " + CreativeTab.getText() + "\n");
+            }
+            if(!itemrotation.getText().isEmpty()){
+                fw.write("ItemRotate: " + itemrotation.getText() + "\n");
+            }
+            if(!itemtranslation.getText().isEmpty()){
+                fw.write("ItemTranslate: " + itemtranslation.getText() + "\n");
+            }
+            if(!itemscale.getText().isEmpty()){
+                fw.write("ItemScale: " + itemscale.getText() + "\n");
+            }
+            if(!IconText.getText().isEmpty()){
+                fw.write("IconText: " + IconText.getText() + "\n");
+            }
+            fw.close();
+            Stage stage = (Stage) name.getScene().getWindow();
+            stage.close();
+            MainController.mainController.refresh();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
